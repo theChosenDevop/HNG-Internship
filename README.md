c39f914f7c3add4e38b4c1a1c06405a8f273a1ef
Oluwatobi Adesanya <tobbey.adesanya@gmail.com>
2024-07-02 20:29:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Second stage of internship
+que
+
+Stage One Task - Backend Track
+Task Description
+Set up a basic web server in your preferred stack. Deploy it to any free hosting platform and expose an API endpoint that conforms to the criteria below:
+Endpoint: [GET] <example.com>/api/hello?visitor_name="Mark" (where <example.com> is your server origin)
+Response:
+{
+  "client_ip": "127.0.0.1", // The IP address of the requester
+  "location": "New York" // The city of the requester
+  "greeting": "Hello, Mark!, the temperature is 11 degrees Celcius in New York"
+}
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const key = process.env.API_KEY;
+
+app.set("trust proxy", true);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/api/hello", async (req, res) => {
+  try {
+    const key = process.env.API_KEY;
+    const visitor = req.query.visitor_name || 'visitor';
+    const base_url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=location&q=current`;
+    const response = await axios.get(base_url);
+    const country = response.data.location.country;
+    const temp = response.data.current.temp_c;
+    const ip = req.ip === '::1' ? '127.0.0.1' : req.ip;
+    res.json({
+      client_ip: ip,
+      location: country,
+      greeting: `Hello, ${visitor}, the temperature is ${temp} degrees Celsius in ${country}`,
+    });
+  } catch (error) {
+    console.error("Error making GET request:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// Base URL: http://api.weatherapi.com/v1
+// /ip.json or /ip.xml
