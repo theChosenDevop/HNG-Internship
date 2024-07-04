06dea19a831d6592fbc55233fe972e94b910a39f
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-04 11:07:36 +0100

    update

--- public/api/app.js
+++ public/api/app.js
@@ -3,28 +3,25 @@ const app = express();
 const axios = require("axios");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const key = process.env.API_KEY;
+const key = "ad0c1a844ca749f2aa1211322240107";
+
 
 app.set("trust proxy", true);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 app.get("/api/hello", async (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const visitor = req.query.visitor_name || 'Mark';
   try {
-    const key = process.env.API_KEY;
-    const visitor = req.query.visitor_name || 'visitor';
     const base_url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=location&q=current`;
     const response = await axios.get(base_url);
     const country = response.data.location.country;
     const temp = response.data.current.temp_c;
-    const ip = req.ip === '::1' ? '127.0.0.1' : req.ip;
+    
     res.json({
-      client_ip: ip,
-      location: country,
-      greeting: `Hello, ${visitor}!, the temperature is ${temp} Celcius in ${country}`,
-    });
+      "client_ip": ip, // The IP address of the requester
+      "location": country, // The city of the requester
+      "greeting": `Hello, ${visitor}!, the temperature is ${temp} degrees Celcius in ${country}`
+    })
   } catch (error) {
     console.error("Error making GET request:", error);
     res.status(500).send("Internal Server Error");
