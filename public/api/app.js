9f7b323d0f8b3e6c191b0e4e70be89a48273e03d
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-04 15:25:53 +0100

    updated location route

--- public/api/app.js
+++ public/api/app.js
@@ -14,13 +14,16 @@ app.get("/api/hello", async (req, res) => {
   try {
     const base_url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=location&q=current`;
     const response = await axios.get(base_url);
-    const country = response.data.location.country;
+    // const country = response.data.location.country;
+    const geoResponse = await axios.get(`https://ipapi.co/${ip}/json`);
+    const geoData = geoResponse.data;
+    const city = geoData.city || 'New york';
     const temp = response.data.current.temp_c;
     
     res.json({
       "client_ip": ip, // The IP address of the requester
-      "location": country, // The city of the requester
-      "greeting": `Hello, ${visitor}!, the temperature is ${temp} degrees Celcius in ${country}`
+      "location": city, // The city of the requester
+      "greeting": `Hello, ${visitor}!, the temperature is ${temp} degrees Celcius in ${city}`
     })
   } catch (error) {
     console.error("Error making GET request:", error);
